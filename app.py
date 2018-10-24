59bbdaafe5cc8b443ce87fea98bc47c7a752a09b
sid22 <siddharth.game@gmail.com>
2018-10-24 23:30:57 +0530

    smile/notsmile added

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ import json
 import pickle
 
 from flask import Flask, Response, render_template, request, url_for
-
+from PIL import Image
 from models.model1 import model1_predictor
 from models.model2 import model2_predictor
+from models.model3 import model3_predictor
 
 app = Flask(__name__)
+
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 @app.route('/')
 def hello_world():
@@ -27,6 +31,7 @@ def api1():
             }
             js = json.dumps(data)
             resp = Response(js, status=200, mimetype='application/json')
+            del fname
             return resp
         except Exception as e:
             data = {
@@ -34,6 +39,7 @@ def api1():
             }
             js = json.dumps(data)
             resp = Response(js, status=400, mimetype='application/json')
+            del fname
             return resp
     elif request.method == 'GET':
         return render_template('cat.html')
@@ -61,6 +67,32 @@ def api2():
             return resp
     elif request.method == 'GET':
         return render_template('hand.html')
+
+@app.route('/smilenotsmile', methods=['GET', 'POST'])
+def api3():
+    if request.method == 'POST':
+        email = request.form.get('email','')
+        fname = request.files['image']
+        print(type(fname))
+        res = Response()
+        try:
+            p = model3_predictor(fname)
+            a = int(p[0])
+            data = {
+                'prediction': a
+            }
+            js = json.dumps(data)
+            resp = Response(js, status=200, mimetype='application/json')
+        except Exception as e:
+            data = {
+                'error_message': "some error occured, ensure your image is small and jpg"
+            }
+            js = json.dumps(data)
+            resp = Response(js, status=400, mimetype='application/json')
+        
+        return resp
+    elif request.method == 'GET':
+        return render_template('smilenotsmile.html')
 
 # with app.test_request_context():
 #     print(url_for('hello_world'))
---
+++ models/model3.py
@@ -0,0 +1,31 @@
+import numpy as np
+from PIL import Image
+from keras.models import load_model
+from keras.models import Model
+from keras.preprocessing import image as kimg
+from keras.applications.imagenet_utils import preprocess_input
+from keras.models import load_model
+from io import StringIO
+import keras.backend as K
+
+def model3_predictor(iimage):
+    model = load_model('happy.h5')
+    try:
+        img = kimg.load_img(iimage, target_size=(64, 64))
+    except Exception as e:
+        print(e)
+    # try:
+    #     ikm = Image.open(iimage.stream)
+    #     print("ikm")
+    #     img = ikm.resize((64,64))
+    #     print("img")
+    # except Exception as e:
+    #     print(e)
+    x = kimg.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    t = model.predict(x)
+    del img
+    del x
+    K.clear_session()
+    return t
